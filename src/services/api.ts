7fbb1609970b22ad330b7b208fc6e74bf05f40a3
wolkyura <yura02505@gmail.com>
2023-08-05 16:11:56 +0200

    fix: add ts-ignore :)

--- src/services/api.ts
+++ src/services/api.ts
@@ -15,6 +15,7 @@ const get = <T extends Api.Get>(path: T, params?: any) => {
   return axios.get<Api.GetResponse[T]>(`${process.env.API_URL}${path}`, { params, headers: getHeaders() });
 };
 
+// @ts-ignore
 const post = <T extends Api.Post>(path: T, data: Api.PostPayload[T], params?: any) => {
   return axios.post<Api.PostResponse[T]>(`${process.env.API_URL}${path}`, data, { params, headers: getHeaders() });
 };
