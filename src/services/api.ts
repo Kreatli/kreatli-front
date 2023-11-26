e068b53b0438e350dcf718328b06da554bdf347f
wolkyura <yura02505@gmail.com>
2023-11-26 16:43:36 +0100

    remove hardcode token

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,8 +3,7 @@ import axios from 'axios';
 import { Api } from '../typings/api';
 
 const getHeaders = () => {
-  // const token = window.localStorage.getItem('token');
-  const token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NjIxNjQxNzliOTBiMDc3YmM1NjE4NSIsImlhdCI6MTcwMDk5NzcyNH0._ggdcvVyyIrOF7ouQ0APnUQGmRJKUt6XdaJSQGneodA';
+  const token = window.localStorage.getItem('token');
 
   return {
     Accept: 'application/json',
