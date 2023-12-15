71bfd843a5afef089ed979be2550aa329b7877b4
wolkyura <yura02505@gmail.com>
2023-12-15 19:50:39 +0100

    fix: youtube channel url pattern

--- src/constants/validationRules.ts
+++ src/constants/validationRules.ts
@@ -1,6 +1,6 @@
 const EMAIL_PATTERN = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
 const URL_PATTERN = /^(http(s)?:\/\/)[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+$/;
-const YOUTUBE_CHANNEL_URL_PATTERN = /^https?:\/\/(?:www\.)?youtube\.com\/(?:channel|c|user\/\S+|@[\w-]+)\/?[a-zA-Z0-9_-]{0,}$/;
+const YOUTUBE_CHANNEL_URL_PATTERN = /^https?:\/\/(?:www\.)?youtube\.com\/(?:channel|c|user\/\S+|@[\w-]+)\/?[.a-zA-Z0-9_-]{0,}$/;
 const DISCORD_USERNAME_PATTERN = /^.{3,32}#[0-9]{4}$/;
 const INSTAGRAM_USERNAME_PATTERN = /^@[\w.\d-]{1,30}$/;
 const TWITTER_ACCOUNT_URL_PATTERN = /(https:\/\/twitter.com\/(?![a-zA-Z0-9_]+\/)([a-zA-Z0-9_]+))/g;
