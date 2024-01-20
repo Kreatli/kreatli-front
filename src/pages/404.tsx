8908f2f93df78d8c1a68a1889180aa399078be3c
wolkyura <yura02505@gmail.com>
2024-01-20 14:47:17 +0100

    feat: final touches 🎉

---
+++ src/pages/404.tsx
@@ -0,0 +1,27 @@
+import { Button, Chip } from '@nextui-org/react';
+import Head from 'next/head';
+import NextLink from 'next/link';
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <>
+      <Head>
+        <title>Page not found | Kreatli</title>
+        <meta name="description" content="Kreatli" />
+      </Head>
+      <div className="container max-w-screen-lg mx-auto px-6 text-center">
+        <Chip variant="flat" color="secondary" classNames={{ content: 'font-semibold' }}>
+          404
+        </Chip>
+        <h2 className="text-4xl font-semibold my-2">Page not found</h2>
+        <p className="mb-8 text-large text-foreground-500">Sorry, but the page you were looking for could not be found 😢</p>
+        <Button as={NextLink} href="/" color="secondary">
+          Go to home page
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default NotFound;
