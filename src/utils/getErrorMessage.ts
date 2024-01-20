cec5eb63393c98eaa97b4373acbf20ea7682ee8d
wolkyura <yura02505@gmail.com>
2024-01-20 15:37:44 +0100

    fix: improve no token message

--- src/utils/getErrorMessage.ts
+++ src/utils/getErrorMessage.ts
@@ -1,3 +1,7 @@
 export const getErrorMessage = (error: any) => {
+  if (error.response?.data.message === 'No token provided') {
+    return 'Please login to continue';
+  }
+
   return error.response?.data.message ?? 'Something went wrong';
 };
